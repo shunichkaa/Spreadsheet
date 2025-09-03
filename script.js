8ab785d04122e21e06a534507b7c49e008ee9c92
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-03 18:57:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-
+const charRange = (start, end) => range(start, end);
 
 window.onload = () => {
   const container = document.getElementById("container");
