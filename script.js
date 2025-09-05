530f5abd66a698ccf2ad27f6bcc84f1fe6e0f349
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-05 19:13:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 const average = nums => sum(nums) / nums.length;
 
-const median = (nums) => {
+const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
   const length = sorted.length;
-  const middle = Math.floor(length / 2);
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
 
-  if (isEven(length)) {
-    return (sorted[middle - 1] + sorted[middle]) / 2;
-  } else {
-    return sorted[middle];
-  }
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
