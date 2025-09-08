e36e422dc34bdf86ae7b60869687740b959fc200
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-08 18:43:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+
   const elemValue = num => {
     const inner = character => {
       return idToText(character + num);
     }
     return inner;
   }
-  const addCharacters = character1 => character2 => {
-
+  const addCharacters = character1 => character2 => num => {
   }
 }
 
