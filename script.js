8c60c4fb9fe20361801973afadcb8cdae20dbc39
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-09 11:30:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,10 @@ const evalFormula = (x, cells) => {
   );
 
   const cellRegex = /[A-J][1-9][0-9]?/gi;
-  const cellExpanded = rangeExpanded.replace(cellRegex, (match) => {});
+  const cellExpanded = rangeExpanded.replace(
+    cellRegex,
+    match => idToText(match.toUpperCase())
+  );
 }
 
 window.onload = () => {
