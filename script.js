459fbe03f6fa6827ab81edf2adc0b962de6ee925
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-10 12:56:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 }
 
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y
+};
+
 const infixEval = (str, regex) =>
   str.replace(regex, (_match, arg1, operator, arg2) =>
-    infixToFunction[operator](arg1, arg2)
+    infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
   );
 
 const isEven = num => num % 2 === 0;
