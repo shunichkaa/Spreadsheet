5d53a147dc665721db28664a93666433a05b09cd
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-11 12:07:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ const applyFunction = str => {
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
 
-  const apply = (fn, args) => 
-    spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, () => {});
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
