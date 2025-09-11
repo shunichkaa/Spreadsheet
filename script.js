ea9fae0a4317c8ba002676ebc529881748e8d013
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-11 12:02:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,15 @@ const spreadsheetFunctions = {
   average,
   median
 }
+
 const applyFunction = str => {
   const noHigh = highPrecedence(str);
   const infix = /([\d.]+)([+-])([\d.]+)/;
   const str2 = infixEval(noHigh, infix);
-
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
-};
+  const toNumberList = args => args.split(",").map(parseFloat);
+
+}
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
